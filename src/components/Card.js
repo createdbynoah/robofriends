b72d09e4e886767689ab20062c8b0eb437a67a26
Noah Rodgers <noah@createdbynoah.com>
2023-10-08 16:01:08 -0500

    initial commit, add tailwind css, card component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src="https://robohash.org/jane?200x200" alt="" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
